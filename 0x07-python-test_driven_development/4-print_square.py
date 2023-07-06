77447bb107569c4349b735c204497a6ac9c77abb
Mamoholane <mamoholanemogotlane@gmail.com>
2023-07-06 05:30:34 -0700

    Updated Mandatory and Advanced Tasks

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-# 4-print_square.py
-"""Defines a square-printing function."""
-
-
 def print_square(size):
-    """Print a square with the # character.
-
-    Args:
-        size (int): The height/width of the square.
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0
-    """
-    if not isinstance(size, int):
+    if type(size) != int:
         raise TypeError("size must be an integer")
-    if size < 0:
+    elif size < 0:
         raise ValueError("size must be >= 0")
-
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    elif type(size) == float:
+        raise TypeError("size must be an integer")
+    for i in range(0, size):
+        for j in range(size):
+            print("#", end= '')
+        print('')i
 
 
--- 0x07-python-test_driven_development/6-max_integer_test.py
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
-
-
 def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
@@ -15,5 +11,5 @@ def max_integer(list=[]):
         if list[i] > result:
             result = list[i]
         i += 1
-    return result
+    return resulti
 
